7fcb92483eb8db1e74c2402ff5c12d698befb9de
Gerrit Koppe <gkoppe@sievers-group.com>
2022-05-29 15:08:21 +0200

    Deleted unnecessary files

--- Test Gerrit/Menu/menubase.h
+++
@@ -1,3 +0,0 @@
-int selectMenu(int lowerY, int upperY, int menuX, int skip);
-void checkBounds (int *playerY, int upperY, int upperX);
-void cursorCallback(int y, int *playerY, int upperY, int lowerY, int menuX);
