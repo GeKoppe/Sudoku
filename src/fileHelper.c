1d31d5f27b69353a2d6b065af6069d7dff72dd48
Jannik Glane <jannikglane@posteo.de>
2022-06-20 17:33:17 +0200

    fix: extension is already passed with filename

--- src/fileHelper.c
+++ src/fileHelper.c
@@ -62,7 +62,6 @@ void buildFilePath(char *fileName, char *pathVariable, char *directory)
 {
     strcat(pathVariable, directory);
     strcat(pathVariable, fileName);
-    strcat(pathVariable, ".txt");
 }
 
 /**
